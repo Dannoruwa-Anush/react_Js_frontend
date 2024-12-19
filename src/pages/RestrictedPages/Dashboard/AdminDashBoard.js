0da4a800ea216d489e0500f31b833bc926deb9bb
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2024-12-19 11:05:02 +0530

    style is updated

--- src/pages/RestrictedPages/Dashboard/AdminDashBoard.js
+++ src/pages/RestrictedPages/Dashboard/AdminDashBoard.js
@@ -5,35 +5,34 @@ import { Nav } from "react-bootstrap";
 const AdminDashBoard = () => {
   const [activeTab, setActiveTab] = useState(0);
 
+  //common tab components
   const tabs = [
-    { id: 0, title: "Tab 1", component: <CategoryTabContent /> },
+    { id: 0, title: "Add Book Category", component: <CategoryTabContent /> },
   ];
+
 
   return (
     <div className="d-flex">
-      {/* Sidebar for Tabs */}
-      <Nav
-        className="flex-column border-end"
-        style={{ width: "25%", minHeight: "100vh" }}
-      >
+
+      {/* [Start] - Sidebar */}
+      <Nav className="nav-sidebar">
+        {/* [Start] - Tabs in the sidebar */}
         {tabs.map((tab, index) => (
           <Nav.Link
             key={tab.id}
             onClick={() => setActiveTab(index)}
             className={activeTab === index ? "active" : ""}
-            style={{
-              padding: "15px",
-              cursor: "pointer",
-              backgroundColor: activeTab === index ? "#e9ecef" : "transparent",
-            }}
           >
             {tab.title}
           </Nav.Link>
         ))}
+        {/* [End] - Tabs in the sidebar */}
       </Nav>
+      {/* [End] - Sidebar*/}
+
 
       {/* Render Tab-Specific Component */}
-      <div className="p-4" style={{ width: "75%" }}>
+      <div className="p-4" style={{ width: "80%" }}>
         {tabs[activeTab].component}
       </div>
     </div>
