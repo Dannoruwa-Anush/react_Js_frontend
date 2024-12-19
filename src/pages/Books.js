325d9049e2ca5f554c4f0dd6d494574199c870a1
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2024-12-19 11:34:01 +0530

    Books page style is modified

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -65,136 +65,136 @@ const Books = () => {
     };
 
     return (
-        <Container fluid>
-            <Row>
-                {/* Left column for vertical navbar */}
-                <Col md={2} className="vertical-navbar">
-                    <Navbar bg="light" expand="md" className="flex-column">
-                        <Nav className="flex-column">
+        <div className="d-flex">
 
-                            {/* Categories Dropdown */}
+            {/* [Start] - Sidebar */}
+            <Nav className="nav-sidebar">
+                {/* [Start] - Tabs in the sidebar */}
+                {/* Categories Dropdown */}
+                <Dropdown
+                    onMouseEnter={() => setExpandedDropdown('categories')}
+                    onMouseLeave={() => setExpandedDropdown(null)}
+                    className="mb-3"
+                >
+                    <Dropdown.Toggle variant="light" id="categories-dropdown" className="w-100 text-start">
+                        Categories
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu show={expandedDropdown === 'categorySubCategoryDetails'}>
+                        {categorySubCategoryDetails && categorySubCategoryDetails.map((category, index) => (
                             <Dropdown
-                                onMouseEnter={() => setExpandedDropdown('categories')}
-                                onMouseLeave={() => setExpandedDropdown(null)}
-                                className="mb-3"
+                                key={index}
+                                onMouseEnter={() => setExpandedSubDropdown(category.categoryName)}
+                                onMouseLeave={() => setExpandedSubDropdown(null)}
+                                className="category-item mb-2"
                             >
-                                <Dropdown.Toggle variant="light" id="categories-dropdown" className="w-100 text-start">
-                                    Categories
+                                <Dropdown.Toggle
+                                    variant="light"
+                                    id={`subcategory-dropdown-${index}`}
+                                    className="w-100 text-start ps-3"
+                                >
+                                    <span
+                                        key={index}
+                                        onClick={(e) => {
+                                            e.preventDefault();  // Prevent default behavior
+                                            handleSelectCategory(category.id);
+                                        }}
+                                    >
+                                        {category.categoryName.toLowerCase()}
+                                    </span>
                                 </Dropdown.Toggle>
-                                <Dropdown.Menu show={expandedDropdown === 'categorySubCategoryDetails'}>
-                                    {categorySubCategoryDetails && categorySubCategoryDetails.map((category, index) => (
-                                        <Dropdown
-                                            key={index}
-                                            onMouseEnter={() => setExpandedSubDropdown(category.categoryName)}
-                                            onMouseLeave={() => setExpandedSubDropdown(null)}
-                                            className="category-item mb-2"
-                                        >
-                                            <Dropdown.Toggle
-                                                variant="light"
-                                                id={`subcategory-dropdown-${index}`}
-                                                className="w-100 text-start ps-3"
-                                            >
-                                                <span
-                                                    key={index}
-                                                    onClick={(e) => {
-                                                        e.preventDefault();  // Prevent default behavior
-                                                        handleSelectCategory(category.id);
-                                                    }}
-                                                >
-                                                    {category.categoryName.toLowerCase()}
-                                                </span>
-                                            </Dropdown.Toggle>
 
-                                            <Dropdown.Menu show={expandedSubDropdown === category.categoryName}>
-                                                {category.subCategories && category.subCategories.map((sub, subIndex) => (
-                                                    <Dropdown.Item as="button" key={index} onClick={(e) => {
-                                                        e.preventDefault();  // Prevent default behavior
-                                                        handleSelectSubCategory(sub.id);
-                                                    }}>
-                                                        {sub.subCategoryName.toLowerCase()}
-                                                    </Dropdown.Item>
-                                                ))}
-                                            </Dropdown.Menu>
-                                        </Dropdown>
+                                <Dropdown.Menu show={expandedSubDropdown === category.categoryName}>
+                                    {category.subCategories && category.subCategories.map((sub, subIndex) => (
+                                        <Dropdown.Item as="button" key={index} onClick={(e) => {
+                                            e.preventDefault();  // Prevent default behavior
+                                            handleSelectSubCategory(sub.id);
+                                        }}>
+                                            {sub.subCategoryName.toLowerCase()}
+                                        </Dropdown.Item>
                                     ))}
                                 </Dropdown.Menu>
                             </Dropdown>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
 
-                            {/* Authors Dropdown */}
-                            <Dropdown
-                                onMouseEnter={() => setExpandedDropdown('authors')}
-                                onMouseLeave={() => setExpandedDropdown(null)}
-                                className="mb-3"
-                            >
-                                <Dropdown.Toggle variant="light" id="authors-dropdown" className="w-100 text-start">
-                                    Authors
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu show={expandedDropdown === 'authorDetails'}>
-                                    <ul className="list-unstyled ps-3">
-                                        {authorDetails && authorDetails.map((author, index) => (
-                                            <Dropdown.Item as="button" key={index} onClick={(e) => {
-                                                e.preventDefault();  // Prevent default behavior
-                                                handleSelectAuthor(author.id);
-                                            }}>
-                                                {author.authorName.toLowerCase()}
-                                            </Dropdown.Item>
-                                        ))}
-                                    </ul>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Nav>
-                    </Navbar>
-                </Col>
-
-                {/* Right column for book details */}
-                <Col md={10} style={{ padding: '1rem' }}>
-                    {bookDetails.length === 0 ? (
-                        <p>No books are available</p>
-                    ) : (
-                        <Row>
-                            {bookDetails && bookDetails.map((book) => (
-                                <Col sm={6} md={4} lg={3} key={book.id} className="mb-4">
-                                    <Card style={{ height: '100%' }}>
-                                        {book.qoh === 0 ? (
-                                            <>
-                                                <Card.Img
-                                                    className="card-books-image"
-                                                    variant="top"
-                                                    src={`${API_IMAGE_URL}/${book.coverImage}`}
-                                                    height={200}
-                                                    width={300}
-                                                />
-                                                <Card.Body className="books-image-card-body">
-                                                    <Card.Title className="books-title">{book.title}</Card.Title>
-                                                    <Card.Text className="books-price">Rs. {book.unitPrice}</Card.Text>
-                                                    <Card.Text style={{ color: 'red', textAlign: 'center' }}>
-                                                        Out of Stock
-                                                    </Card.Text>
-                                                </Card.Body>
-                                            </>
-                                        ) : (
-                                            <Link to={`/book/${book.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                                <Card.Img
-                                                    className="card-books-image"
-                                                    variant="top"
-                                                    src={`${API_IMAGE_URL}/${book.coverImage}`}
-                                                    height={200}
-                                                    width={300}
-                                                />
-                                                <Card.Body className="books-image-card-body">
-                                                    <Card.Title className="books-title">{book.title}</Card.Title>
-                                                    <Card.Text className="books-price">Rs. {book.unitPrice}</Card.Text>
-                                                </Card.Body>
-                                            </Link>
-                                        )}
-                                    </Card>
-                                </Col>
+                {/* Authors Dropdown */}
+                <Dropdown
+                    onMouseEnter={() => setExpandedDropdown('authors')}
+                    onMouseLeave={() => setExpandedDropdown(null)}
+                    className="mb-3"
+                >
+                    <Dropdown.Toggle variant="light" id="authors-dropdown" className="w-100 text-start">
+                        Authors
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu show={expandedDropdown === 'authorDetails'}>
+                        <ul className="list-unstyled ps-3">
+                            {authorDetails && authorDetails.map((author, index) => (
+                                <Dropdown.Item as="button" key={index} onClick={(e) => {
+                                    e.preventDefault();  // Prevent default behavior
+                                    handleSelectAuthor(author.id);
+                                }}>
+                                    {author.authorName.toLowerCase()}
+                                </Dropdown.Item>
                             ))}
-                        </Row>
-                    )}
-                </Col>
-            </Row>
-        </Container>
+                        </ul>
+                    </Dropdown.Menu>
+                </Dropdown>
+                {/* [End] - Tabs in the sidebar */}
+            </Nav>
+            {/* [End] - Sidebar*/}
+
+
+            {/* [Start] - Main Content */}
+            <div className="main-content">
+                {/* Book Details*/}
+                {bookDetails.length === 0 ? (
+                    <p>No books are available</p>
+                ) : (
+                    <Row>
+                        {bookDetails && bookDetails.map((book) => (
+                            <Col sm={6} md={4} lg={3} key={book.id} className="mb-4">
+                                <Card style={{ height: '100%' }}>
+                                    {book.qoh === 0 ? (
+                                        <>
+                                            <Card.Img
+                                                className="card-books-image"
+                                                variant="top"
+                                                src={`${API_IMAGE_URL}/${book.coverImage}`}
+                                                height={200}
+                                                width={300}
+                                            />
+                                            <Card.Body className="books-image-card-body">
+                                                <Card.Title className="books-title">{book.title}</Card.Title>
+                                                <Card.Text className="books-price">Rs. {book.unitPrice}</Card.Text>
+                                                <Card.Text style={{ color: 'red', textAlign: 'center' }}>
+                                                    Out of Stock
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </>
+                                    ) : (
+                                        <Link to={`/book/${book.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                            <Card.Img
+                                                className="card-books-image"
+                                                variant="top"
+                                                src={`${API_IMAGE_URL}/${book.coverImage}`}
+                                                height={200}
+                                                width={300}
+                                            />
+                                            <Card.Body className="books-image-card-body">
+                                                <Card.Title className="books-title">{book.title}</Card.Title>
+                                                <Card.Text className="books-price">Rs. {book.unitPrice}</Card.Text>
+                                            </Card.Body>
+                                        </Link>
+                                    )}
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                )}
+            </div>
+            {/* [End] - Main Content */}
+        </div>
     );
 };
 
