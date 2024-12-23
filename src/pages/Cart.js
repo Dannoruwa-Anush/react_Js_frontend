e610aa71151f402897e7492f710a976904afa70d
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2024-12-23 11:00:54 +0530

    style is updated

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -134,110 +134,110 @@ const Cart = () => {
     return (
         <Container className="mt-4">
             <h5>Shopping Cart</h5>
-            {cartItems.length === 0 ? (
-                <p>Your cart is empty.</p>
-            ) : (
-                <Table bordered striped hover responsive className="book-info-table">
-                    <thead>
-                        <tr>
-                            <th>Item No</th>
-                            <th>Item</th>
-                            <th>Description</th>
-                            <th>Price</th>
-                            <th>Qty</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cartItems.map(item => (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>
-                                    <img src={`${API_IMAGE_URL}/${item.image}`} alt={item.title} style={{ width: '100px' }} />
-                                </td>
-                                <td>
-                                    <h5>{item.name}</h5>
-                                </td>
-                                <td>
-                                    <p>Rs. {item.price}</p>
-                                </td>
-                                <td>
-                                    <input
-                                        type="number"
-                                        value={item.quantity}
-                                        min="1"
-                                        max={item.qoh}
-                                        onChange={(e) => handleQuantityChange(item.id, e.target.value)}
-                                    />
-                                </td>
-                                <td>
-                                    <Button variant="danger" onClick={() => handleRemove(item.id)}
-                                        style={{
-                                            borderRadius: '30px',
-                                            width: '100px',
-                                            height: '40px'
-                                        }}>
-                                        Remove
-                                    </Button>
-                                </td>
+            <div className="main-content-table-container">
+                {cartItems.length === 0 ? (
+                    <p>Your cart is empty.</p>
+                ) : (
+
+                    <Table bordered striped hover responsive>
+                        <thead>
+                            <tr>
+                                <th>Item No</th>
+                                <th>Item</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th>Qty</th>
+                                <th className="main-content-table-action-column">Action</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            )}
+                        </thead>
+                        <tbody>
+                            {cartItems.map(item => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>
+                                        <img src={`${API_IMAGE_URL}/${item.image}`} alt={item.title} style={{ width: '100px' }} />
+                                    </td>
+                                    <td>{item.name}</td>
+                                    <td>Rs. {item.price}</td>
+                                    <td className="cart-table-qty-column">
+                                        <input
+                                            type="number"
+                                            value={item.quantity}
+                                            min="1"
+                                            max={item.qoh}
+                                            onChange={(e) => handleQuantityChange(item.id, e.target.value)}
+                                            className='cart-table-qty-input-field'
+                                        />
+                                    </td>
+                                    <td className="main-content-table-action-column">
+                                        <Button variant="outline-danger" size="sm" onClick={() => handleRemove(item.id)}
+                                            style={{
+                                                borderRadius: '30px',
+                                                width: '100px',
+                                                height: '40px'
+                                            }}>
+                                            Remove
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
 
-            {cartItems.length > 0 && (
-                <div className='div-cart-total-amount'>
-                    <span>Total: Rs. {cartTotalAmount}</span>
-                </div>
-            )}
+                )}
 
-            {cartItems.length > 0 && (
-                <div className="d-flex justify-content-end">
-                    <Button variant="success" onClick={handlePlaceOrder} className="mt-3"
-                        style={{
-                            borderRadius: '30px',
-                        }}>
-                        Place Order
-                    </Button>
-                </div>
-            )}
+                {cartItems.length > 0 && (
+                    <div className='div-cart-total-amount'>
+                        <span>Total: Rs. {cartTotalAmount}</span>
+                    </div>
+                )}
 
-            {/* Confirmation Modal for Item Removal */}
-            <Modal show={showModal} onHide={cancelRemove}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Confirm Removal</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Are you sure you want to remove this item from your cart?
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={cancelRemove}>
-                        Cancel
-                    </Button>
-                    <Button variant="danger" onClick={confirmRemove}>
-                        Confirm Remove
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                {cartItems.length > 0 && (
+                    <div className="d-flex justify-content-end">
+                        <Button variant="primary" onClick={handlePlaceOrder} className="button-style"
+>
+                            Place Order
+                        </Button>
+                    </div>
+                )}
 
-            {/* Login Modal for Placing Order */}
-            <Modal show={showLoginModal} onHide={cancelLogin}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Login Required</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    You must be logged in to place an order. Would you like to log in now?
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={cancelLogin}>
-                        Cancel
-                    </Button>
-                    <Button variant="primary" onClick={handleLogin}>
-                        Login
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                {/* Confirmation Modal for Item Removal */}
+                <Modal show={showModal} onHide={cancelRemove}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Confirm Removal</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Are you sure you want to remove this item from your cart?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={cancelRemove}>
+                            Cancel
+                        </Button>
+                        <Button variant="danger" onClick={confirmRemove}>
+                            Confirm Remove
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                {/* Login Modal for Placing Order */}
+                <Modal show={showLoginModal} onHide={cancelLogin}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Login Required</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        You must be logged in to place an order. Would you like to log in now?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={cancelLogin}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" onClick={handleLogin}>
+                            Login
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </div>
         </Container>
     );
 };
